ac5512063d8ff14f0c04313e76d03301785b34eb
Tracey Young <tracey.y.ltp@gmail.com>
2023-01-02 20:22:25 +1300

    Fixed bug in hasWon method

--- BIT504_Assign2_PartAStudent/Board.java
+++ BIT504_Assign2_PartAStudent/Board.java
@@ -52,9 +52,10 @@ public class Board {
 		if(cells[playerRow][0].content == thePlayer && cells[playerRow][1].content == thePlayer && cells[playerRow][2].content == thePlayer )
 			return true; 
 		
-		if(cells[playerCol][0].content == thePlayer && cells[playerCol][1].content == thePlayer && cells[playerRow][2].content == thePlayer)
+		// check if player has 3-in-that-column
+		if(cells[0][playerCol].content == thePlayer && cells[1][playerCol].content == thePlayer && cells[2][playerCol].content == thePlayer)
 			return true;
-		 // TODO: Check if the player has 3 in the playerCol.  done
+		 
 		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column] 
 		
 		
